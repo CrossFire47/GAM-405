d45c28563daa9ce350a31c01f20a2903fc76225c
CrossFire47 <kamwal2007@gmail.com>
2025-12-06 20:14:18 +0000

    Cool Password Developments

--- Assets/Scripts/PasswordScrip.cs
+++ Assets/Scripts/PasswordScrip.cs
@@ -24,14 +24,71 @@ public class PasswordScrip : MonoBehaviour
     {
         btnClicked = 0;
         numOfGuesses = correctPassword.Length;
+        displayText.SetActive(false);
     }
 
     
     void Update()
     {
-        if(input == correctPassword)
+        if(btnClicked == numOfGuesses)
         {
+            if (input == correctPassword)
+            {
+                Debug.Log("Correct Password!");
+                input = "";
+                btnClicked = 0;
+            }
+            else
+            {
+                input = "";
+                displayText.text = input.ToString();
+                btnClicked = 0;
+            }
+        }  
             
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.CompareTag("Player"))
+        {
+            passwordScreen = true;
+
+            if (passwordScreen)
+            {
+                objectToDisable.SetActive(false);
+                objectToDisable2.SetActive(false);
+                objectToEnable.SetActive(true);
+            }
+        }
+    }
+
+    public void ValueEntered(string valueEntered)
+    {
+        switch (valueEntered)
+        {
+            case "Q": //QUIT
+                objectToDisable.SetActive(true);
+                objectToDisable2.SetActive(true);
+                objectToEnable.SetActive(false);
+                btnClicked = 0;
+                passwordScreen = false;
+                input = "";
+                displayText.text = input.ToString();
+                break;
+
+            case "C": //CLEAR
+                input = "";
+                btnClicked = 0; //Clear Guess Count
+                displayText.text = input.ToString();
+                break;
+
+            default: // Button clicked add a variable
+                btnClicked++;
+                input += valueEntered;
+                displayText.text = input.ToString();
+                break;    
+
         }
     }
 }
